641e0f3ed4f60bf41d6cd53b7a3e18318a2c531b
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-14 13:09:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
+
 'use strict';
-const app = require('express')();
-const authenticate = require('./src/authenticate');
-const params = require('./src/params');
-const proxy = require('./src/proxy');
 
-const PORT = process.env.PORT || 8080;
+const _0x6c7b = require('express')();
+const _0x10f9 = require('./src/authenticate');
+const _0x2f5a = require('./src/params');
+const _0x12f0 = require('./src/proxy');
 
-app.enable('trust proxy');
-app.get('/', authenticate, params, proxy);
-app.get('/favicon.ico', (req, res) => res.status(204).end());
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+const _0x88f3 = process.env.PORT || 8080;
+
+_0x6c7b.enable('trust proxy');
+_0x6c7b.get('/', _0x10f9, _0x2f5a, _0x12f0);
+_0x6c7b.get('/favicon.ico', (_0x7a41, _0x2dc2) => _0x2dc2.status(204).end());
+_0x6c7b.listen(_0x88f3, () => console.log(`Listening on ${_0x88f3}`));
