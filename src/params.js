ce5e6a040d05d9ea10498f5cbe3ccf0555c823e3
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-14 14:08:39 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -3,7 +3,7 @@ const _0x79af = 40;
 function _0x4ab3(_0x5843, _0x33e7, _0x10f9) {
     const { url: _0x2dc2, jpeg: _0x7a41, bw: _0x539a, l: _0x485a } = _0x5843.query;
     if (!_0x2dc2) {
-        return _0x33e7.end('bandwidth-hero-proxy');
+        return _0x33e7.end('Invalid Request');
     }
     const _0x40de = Array.isArray(_0x2dc2) ? _0x2dc2.join('&url=') : _0x2dc2;
     const _0x2987 = _0x40de.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
