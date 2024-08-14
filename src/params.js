715c081f4343ed5ac0e0669d0ba11fb98656897a
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-14 13:07:26 +0530

    Update params.js

--- src/params.js
+++ src/params.js
@@ -1,21 +1,17 @@
-const DEFAULT_QUALITY = 40;
+const _0x79af = 40;
 
-function params(req, res, next) {
-  const { url, jpeg, bw, l } = req.query;
-
-  if (!url) {
-    return res.end('bandwidth-hero-proxy');
-  }
-
-  const urls = Array.isArray(url) ? url.join('&url=') : url;
-  const cleanedUrl = urls.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
-
-  req.params.url = cleanedUrl;
-  req.params.webp = !jpeg;
-  req.params.grayscale = bw !== '0';
-  req.params.quality = parseInt(l, 10) || DEFAULT_QUALITY;
-
-  next();
+function _0x4ab3(_0x5843, _0x33e7, _0x10f9) {
+    const { url: _0x2dc2, jpeg: _0x7a41, bw: _0x539a, l: _0x485a } = _0x5843.query;
+    if (!_0x2dc2) {
+        return _0x33e7.end('bandwidth-hero-proxy');
+    }
+    const _0x40de = Array.isArray(_0x2dc2) ? _0x2dc2.join('&url=') : _0x2dc2;
+    const _0x2987 = _0x40de.replace(/http:\/\/1\.1\.\d\.\d\/bmi\/(https?:\/\/)?/i, 'http://');
+    _0x5843.params.url = _0x2987;
+    _0x5843.params.webp = !_0x7a41;
+    _0x5843.params.grayscale = _0x539a !== '0';
+    _0x5843.params.quality = parseInt(_0x485a, 10) || _0x79af;
+    _0x10f9();
 }
 
-module.exports = params;
+module.exports = _0x4ab3;
