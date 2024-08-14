0c22f7200452b796d26edee5eebf38eeca41dff3
Protoman1234 <rffrgtdgd@gmail.com>
2024-08-14 13:06:41 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,27 +1,21 @@
-const sharp = require('sharp');
-const redirect = require('./redirect');
+const _0x5843 = require('sharp');
+const _0x7a41 = require('./redirect');
 
-function compress(req, res, input) {
-  const format = req.params.webp ? 'webp' : 'jpeg';
-
-  sharp(input)
-    .grayscale(req.params.grayscale)
-    .toFormat(format, {
-      quality: req.params.quality,
-      progressive: true,
-      optimizeScans: true
-    })
-    .toBuffer((err, output, info) => {
-      if (err || !info || res.headersSent) {
-        return redirect(req, res);
-      }
-
-      res.setHeader('content-type', `image/${format}`);
-      res.setHeader('content-length', info.size);
-      res.setHeader('x-original-size', req.params.originSize);
-      res.setHeader('x-bytes-saved', req.params.originSize - info.size);
-      res.status(200).send(output);
-    });
+function _0x4032(_0x2e9f, _0x33e7, _0x485a) {
+    const _0x7f94 = _0x2e9f.params.webp ? 'webp' : 'jpeg';
+    _0x5843(_0x485a)
+        .grayscale(_0x2e9f.params.grayscale)
+        .toFormat(_0x7f94, { quality: _0x2e9f.params.quality, progressive: true, optimizeScans: true })
+        .toBuffer((_0x539a, _0x4f2b, _0x40de) => {
+            if (_0x539a || !_0x40de || _0x33e7.headersSent) {
+                return _0x7a41(_0x2e9f, _0x33e7);
+            }
+            _0x33e7.setHeader('content-type', `image/${_0x7f94}`);
+            _0x33e7.setHeader('content-length', _0x40de.size);
+            _0x33e7.setHeader('x-original-size', _0x2e9f.params.originSize);
+            _0x33e7.setHeader('x-bytes-saved', _0x2e9f.params.originSize - _0x40de.size);
+            _0x33e7.status(200).send(_0x4f2b);
+        });
 }
 
-module.exports = compress;
+module.exports = _0x4032;
